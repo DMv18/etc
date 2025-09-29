0672abe411020cf8c8127a9db18d115656897d4a
DMv18 <152433678+DMv18@users.noreply.github.com>
2025-09-29 19:38:27 +0000

    start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Accordian from '/src/components/accordian';
+
+export function App(props) {
+  return <div className='App'>
+    {/** accordian */} 
+    <Accordian />
+    </div>;
+}
+
+// Log to console
+console.log('Hello console');
---
+++ src/components/accordian/data.js
@@ -0,0 +1,14 @@
+const data = [
+  {
+    id: '1',
+    question: 'a',
+    answer: 'a',
+  },
+  {
+    id: '2',
+    question: 'b',
+    answer: 'b',
+  },
+];
+
+export default data;
